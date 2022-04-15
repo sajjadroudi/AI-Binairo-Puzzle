07a57c2c916a243249f37ebedff61c9b7fd56bc6
Roudi <sajjad.roodi@gmail.com>
2022-04-15 17:09:31 +0430

    Implement AdjacencyCondition

---
+++ src/AdjacencyCondition.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class AdjacencyCondition implements ConditionSatisfication {
+
+    private final int maxAcceptableAdjacentCircles;
+
+    public AdjacencyCondition(int maxAcceptableAdjacentCircles) {
+        this.maxAcceptableAdjacentCircles = maxAcceptableAdjacentCircles;
+    }
+
+    public AdjacencyCondition() {
+        this(2);
+    }
+
+    @Override
+    public boolean doesSatisfy(Board board) {
+        for(int row = 0; row < board.sideCount(); row++) {
+            for(int col = 0; col < board.sideCount(); col++) {
+                Set<Cell> cells = new HashSet<>();
+                for(int i = 0; i <= maxAcceptableAdjacentCircles; i++) {
+                    var value = board.get(row + i, col);
+                    if(value != null)
+                        cells.add(value);
+                }
+
+                if(cells.size() == 1)
+                    return false;
+
+                cells.clear();
+                for(int i = 0; i <= maxAcceptableAdjacentCircles; i++) {
+                    var value = board.get(row, col + i);
+                    if(value != null)
+                        cells.add(value);
+                }
+
+                if(cells.size() == 1)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
