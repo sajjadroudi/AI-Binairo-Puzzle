d6be7f7113951d0047c9c1d254482363173b71dd
Roudi <sajjad.roodi@gmail.com>
2022-04-21 00:54:32 +0430

    Implement LcvHeuristic

--- src/heuristic/LcvHeuristic.java
+++ src/heuristic/LcvHeuristic.java
@@ -1,5 +1,6 @@
 package heuristic;
 
+import core.ForwardCheckingApplier;
 import model.Board;
 import model.Coordinates;
 import model.Value;
@@ -10,11 +11,38 @@ public class LcvHeuristic implements Heuristic {
 
     public List<Value> findAppropriateValue(Board board, Coordinates coordinates) {
         Value[] domain = board.getDomain(coordinates.getRow(), coordinates.getCol());
-        return switch (domain.length) {
-            case 0 -> List.of();
-            case 1 -> List.of(domain[0]);
-            default -> List.of(domain); // TODO
-        };
+        if(domain.length == 0) {
+            return List.of();
+        } else if(domain.length == 1) {
+            return List.of(domain[0]);
+        } else {
+            int white = put(board.copy(), coordinates, Value.WHITE);
+            int black = put(board.copy(), coordinates, Value.BLACK);
+
+            if(white > black) {
+                return List.of(Value.WHITE, Value.BLACK);
+            } else {
+                return List.of(Value.BLACK, Value.WHITE);
+            }
+        }
+    }
+
+    private int put(Board board, Coordinates coordinates, Value value) {
+        board.setValue(coordinates.getRow(), coordinates.getCol(), value);
+
+        var forwardChecking = new ForwardCheckingApplier();
+        forwardChecking.applyForwardChecking(board, coordinates);
+
+        int allDomainSize = 0;
+        for(int row = 0; row < board.sideCount(); row++) {
+            for(int col = 0; col < board.sideCount(); col++) {
+                if(board.getValueOrNull(row, col) == null) {
+                    allDomainSize += board.getDomainSize(row, col);
+                }
+            }
+        }
+
+        return allDomainSize;
     }
 
 }
